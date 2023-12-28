9b365ce54be7997ec31d4bbe7987875d082d73b8
Дмитрий <Avgustinovich.DV@parmalogica.ru>
2023-12-28 18:31:49 +0500

    _

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,20 @@
-import { Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
-import { Link } from "react-router-dom";
-import { Suspense } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "./providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
 
 export const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-
-      <Link to="/">Главная</Link>
-      <Link to="/about">О сайте</Link>
-
-      <Suspense fallback={<>Loading...</>}>
-        <Routes>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </Suspense>
-
-      <h1>Hello World</h1>
+      <Navbar />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -6,3 +6,12 @@ declare module "*.scss" {
   const cssModule: IClassName;
   export = cssModule;
 }
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg" {
+  import React from "react";
+  export const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
