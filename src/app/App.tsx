139377887b0520724bf1f8ebdf49cee0cb9d884f
Дмитрий <Avgustinovich.DV@parmalogica.ru>
2023-12-28 14:36:48 +0500

    _

---
+++ src/app/App.tsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import "./styles/index.scss";
+import { Link } from "react-router-dom";
+import { Suspense } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "./providers/ThemeProvider";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+
+export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+
+      <Link to="/">Главная</Link>
+      <Link to="/about">О сайте</Link>
+
+      <Suspense fallback={<>Loading...</>}>
+        <Routes>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </Suspense>
+
+      <h1>Hello World</h1>
+    </div>
+  );
+};
