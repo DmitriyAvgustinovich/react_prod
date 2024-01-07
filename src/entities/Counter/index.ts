d5c74af0e981a12165b0adfa8ab1e533fc984281
Дмитрий <Avgustinovich.DV@parmalogica.ru>
2024-01-08 02:57:11 +0500

    _

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,5 +1,3 @@
-import { CounterSchema } from "./model/types/counterSchema";
-import { counterReducer } from "./model/slice/counterSlice";
-import { Counter } from "./ui/Counter";
-
-export { counterReducer, Counter, CounterSchema };
+export { CounterSchema } from "./model/types/counterSchema";
+export { counterReducer } from "./model/slice/counterSlice";
+export { Counter } from "./ui/Counter";
